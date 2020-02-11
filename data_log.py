9a7287e01b926929953d7f04fd32995b6fb37c32
Ahmed Ibrahim <ahmed.ms@alumni.ubc.ca>
2020-02-10 16:36:34 -0800

    Create data_log.py

---
+++ data_log.py
@@ -0,0 +1,62 @@
+# Necessary packages
+import sys, traceback, time, os
+import warnings, re
+from datetime import datetime
+
+class DataLog(object):
+
+    def __init__(self, directory='raw_data'):
+        # Class for holding variables' values
+        # Open log file
+        self.raw_data_dir = os.path.abspath(os.path.join(os.path.dirname("__file__"), '..', directory))
+
+    # Data log functions
+    def new_log(self, filename='default'):
+        """
+            Creat's new log file, assign file name and type
+            Parameters:
+                filename: name of log file, if 'default', it will create a name based on test number and mode
+        """
+        # check if log directory exists, if not create it
+        try:
+            os.stat(self.raw_data_dir)
+        except:
+            os.makedirs(self.raw_data_dir)
+
+        # file name
+        self.filename = filename + '.csv'
+
+        # Assign delimiter
+        self.delimiter = ','
+        
+        # Open log file
+        self.fp = open(self.raw_data_dir + "\\" + self.filename, 'a')
+        print("Log file: %s" % self.raw_data_dir + '\\' + self.filename)
+
+        return
+
+    def print_log(self, test_results=None):
+        """
+            Prints header / results
+        """
+        # Create new print_line
+        print_line = '\n'
+        # Appending test header/results line
+        if test_results is not None:
+            print_line += self.delimiter.join(test_results) + self.delimiter
+
+        # If closed, reopen
+        if self.fp.closed:
+            self.fp = open(self.raw_data_dir + "\\" + self.filename, 'a')
+
+        # write header/results line
+        if sys.version_info[0] < 3:
+            self.fp.write(unicode(print_line))
+        else:
+            self.fp.write(str(print_line))
+        self.fp.flush()
+        self.fp.close()
+
+        print("Results successfully logged to %s" % self.filename)
+
+        return
